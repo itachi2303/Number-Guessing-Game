86fc62b35134e70cdd9f61bdde2321ac3d735292
itachi2303 <149932832+itachi2303@users.noreply.github.com>
2024-05-17 02:41:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer  from "inquirer";
+let gessnum =  Math.floor(Math.random() * 10 + 1);
+do{
+let answer = await inquirer.prompt([
+    {
+        message: "(^o^) enter a number between 1-10 : ",
+        type: "number",
+        name: "num1"
+    },
+]);
+function playagain(){
+    if(answer.num1 < gessnum){
+        console.log(" nah u guessed the wrong number, please try again :(");
+    }
+    
+    }
+    if(answer.num1 > gessnum){
+        console.log(" nope u guessed the wrong number, please try again :(");
+    }
+    if(answer.num1 == gessnum){
+        console.log(" (^.^) yayiee you guessed the number correctly :)");
+        process.exit()
+    }
+playagain()
+}
+while(true)
+
+
+
